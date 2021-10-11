c8b99d3bc8fe4a3e992e1a881463796cc9731d6a
frostis-dev <markusslask@gmail.com>
2021-10-11 21:40:19 +0300

    More backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const sqlite3 = require('sqlite3').verbose()
+//const db = sqlite3.Database('./database/coomments.sqlite')
+/*
+db.serialize( () => {
+    //db.run("CREATE TABLE IF NOT EXISTS comments (id int NOT NULL AUTO_INCREMENT, name varchar(35) NOT NULL, comment LONGTEXT NOT NULL);")
+})
+*/
 
 app.post('/comment', (req, res) => {
-    res.send('Viss ir uwu')
+    console.log(req.params)
+    res.redirect('http://127.0.0.1:5500/client')
+
 })
 
 app.listen(5500)
